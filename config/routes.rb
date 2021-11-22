13afd6ef8daced0bcd80bdec8285a93c990ce240
Hiro Mataba <aganzemataba@gmail.com>
2021-11-22 11:52:50 +0200

    Add Categories route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'categories#index'
+  devise_for :users
+
+  resources :categories
 end
